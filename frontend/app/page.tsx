37786db292c4db061d72158a83e454be0f8d755e
Huleji Abraham Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-07-12 15:54:14 -0500

    Add files via upload

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { Component } from "@/components/component";
+// import { Loading } from "@/components/loading";
+
+export default function Home() {
+  return (
+   <Component/>
+  //  <Loading/>
+  );
+}
