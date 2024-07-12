71149fcf0c4049e98fe9acd3e945fca2e58e93cf
Huleji Abraham Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-07-12 16:04:33 -0500

    Delete frontend/app directory

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "@/components/component";
-// import { Loading } from "@/components/loading";
-
-export default function Home() {
-  return (
-   <Component/>
-  //  <Loading/>
-  );
-}
